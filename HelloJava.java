2a3c8247e8a3513b99c988d7cbe466e3de2bead6
ccreglos <82140659+ccreglos@users.noreply.github.com>
2021-06-09 23:02:23 +0800

    Create HelloJava.java

---
+++ HelloJava.java
@@ -0,0 +1,10 @@
+public class HelloJava {
+
+    public static void main(String[] args) {
+        String sentence = "Hello to Java World!!";
+        int count = 12;
+       
+        System.out.println("Sentence " + sentence);
+        System.out.println("Count " + count);
+    }
+}
